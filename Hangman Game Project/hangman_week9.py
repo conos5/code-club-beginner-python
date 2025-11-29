c3936186c686f35e70750147c74d4bbc49870566
Conor Shipsey <conor.shipsey@ibm.com>
2025-11-29 11:40:37 +0000

    Added hangman ascii art to the code

--- Hangman Game Project/hangman_week9.py
+++ Hangman Game Project/hangman_week9.py
@@ -3,6 +3,82 @@ import random
 print("=== HANGMAN ===\n")
 print("Welcome to Hangman!")
 
+# HANGMAN ASCII ART
+# Each wrong guess shows the next stage
+
+HANGMAN_PICS = [
+    # Stage 0 - No wrong guesses
+    """
+       ------
+       |    |
+       |
+       |
+       |
+       |
+    --------
+    """,
+    # Stage 1 - Head
+    """
+       ------
+       |    |
+       |    O
+       |
+       |
+       |
+    --------
+    """,
+    # Stage 2 - Body
+    """
+       ------
+       |    |
+       |    O
+       |    |
+       |
+       |
+    --------
+    """,
+    # Stage 3 - Left arm
+    """
+       ------
+       |    |
+       |    O
+       |   /|
+       |
+       |
+    --------
+    """,
+    # Stage 4 - Right arm
+    """
+       ------
+       |    |
+       |    O
+       |   /|\\
+       |
+       |
+    --------
+    """,
+    # Stage 5 - Left leg
+    """
+       ------
+       |    |
+       |    O
+       |   /|\\
+       |   /
+       |
+    --------
+    """,
+    # Stage 6 - Right leg (GAME OVER)
+    """
+       ------
+       |    |
+       |    O
+       |   /|\\
+       |   / \\
+       |
+    --------
+    """
+]
+
 # Game Setup
 
 # Pick a random word
